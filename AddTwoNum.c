c8c40808a0e8d7a387c2eb876dbe68b3d469b4fc
freeyun <1571216237@qq.com>
2018-02-13 23:01:27 +0800

    first edition

---
+++ AddTwoNum.c
@@ -0,0 +1,66 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+#include <stdio.h>
+#include <stdlib.h>
+struct ListNode
+ {
+      int val;
+      struct ListNode *next;
+ };
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
+{
+  struct ListNode* re = (struct ListNode*)malloc(sizeof(struct ListNode));
+  struct ListNode* ret = re;
+  
+  int carry = 0;
+  int sum = 0;
+  int l1v,l2v;
+  ret->val = (l1->val + l2->val)%10;
+  carry = (l1->val + l2->val)/10; 
+  l1 = l1->next;
+  l2 = l2->next;
+  ret -> next = NULL;
+  while(l1 != NULL || l2 != NULL || carry == 1)
+  {
+        struct ListNode* t = (struct ListNode*)malloc(sizeof(struct ListNode));
+        if(l1 == NULL) {l1v = 0;}else{l1v = l1->val;l1 = l1->next;}
+        if(l2 == NULL) {l2v = 0;}else{l2v = l2->val;l2 = l2->next;}
+        sum = l1v+l2v+carry;     
+        if(sum > 10) {carry = 1;}else{carry = 0;}
+        sum = sum % 10;
+        t->val = sum;  
+        t->next = NULL;
+        ret->next = t;
+        ret = t;
+  }
+  return re;  
+return re;
+}
+int main()
+{
+  struct ListNode* l1 = (struct ListNode*)malloc(sizeof(struct ListNode));
+    struct ListNode* r1 = (struct ListNode*)malloc(sizeof(struct ListNode));
+     struct ListNode* l2 = (struct ListNode*)malloc(sizeof(struct ListNode));
+     struct ListNode* r2 = (struct ListNode*)malloc(sizeof(struct ListNode));
+      l1->val = 2;
+      r1->val = 3;
+      l2->val = 8;
+      r2->val = 1;
+      l1->next = r1;
+      r1->next = NULL;
+      l2->next = r2;
+      r2->next = NULL;
+      struct ListNode* re =addTwoNumbers(l1, l2);
+      while(re)
+{
+ printf("%d",re->val);
+re = re->next;
+}      
+ 
+  return 0;
+}
