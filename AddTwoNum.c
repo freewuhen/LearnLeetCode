55804d3e7c3d34058d54389c0b43871eaa7dbefb
freeyun <1571216237@qq.com>
2018-02-15 09:28:50 +0800

    debug

--- AddTwoNum.c
+++ AddTwoNum.c
@@ -31,7 +31,7 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
         if(l1 == NULL) {l1v = 0;}else{l1v = l1->val;l1 = l1->next;}
         if(l2 == NULL) {l2v = 0;}else{l2v = l2->val;l2 = l2->next;}
         sum = l1v+l2v+carry;     
-        if(sum > 10) {carry = 1;}else{carry = 0;}
+	carry = sum/10;
         sum = sum % 10;
         t->val = sum;  
         t->next = NULL;
@@ -44,17 +44,26 @@ return re;
 int main()
 {
   struct ListNode* l1 = (struct ListNode*)malloc(sizeof(struct ListNode));
-    struct ListNode* r1 = (struct ListNode*)malloc(sizeof(struct ListNode));
+  struct ListNode* r1 = (struct ListNode*)malloc(sizeof(struct ListNode));
+  struct ListNode* r2 = (struct ListNode*)malloc(sizeof(struct ListNode));
+
+
      struct ListNode* l2 = (struct ListNode*)malloc(sizeof(struct ListNode));
-     struct ListNode* r2 = (struct ListNode*)malloc(sizeof(struct ListNode));
+     struct ListNode* r3 = (struct ListNode*)malloc(sizeof(struct ListNode));
+      struct ListNode* r4 = (struct ListNode*)malloc(sizeof(struct ListNode));
+
       l1->val = 2;
-      r1->val = 3;
-      l2->val = 8;
-      r2->val = 1;
+      r1->val = 4;
+      r2->val = 3;
+      l2->val = 5;
+      r3->val = 6;
+      r4->val = 4;
       l1->next = r1;
-      r1->next = NULL;
-      l2->next = r2;
+      r1->next = r2;
       r2->next = NULL;
+      l2->next = r3;
+      r3->next = r4;
+      r4->next = NULL;
       struct ListNode* re =addTwoNumbers(l1, l2);
       while(re)
 {
